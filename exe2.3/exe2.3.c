ea755776206524f74afd4e782416c8c6ea4bcdbc
Dennis Paiva <33735664+jacatau23@users.noreply.github.com>
2018-09-19 10:01:58 -0300

    Create exe2.3.c

---
+++ exe2.3/exe2.3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	char temps[50];
+	int temperatura;
+	int i,soma = 0,media;
+
+	for (i = 0;i<50;i++)
+	{
+		printf("Escreva uma temperatura(celcius): ");
+		scanf("%d",&temperatura);
+
+		while(temperatura < -100 || temperatura > 100)
+		{
+			printf("Temperatura nao valida!\n");
+			printf("Escreva uma temperatura(celcius): ");
+			scanf("%d",&temperatura);
+		}
+		soma += temperatura;
+		printf("%d\n",soma);
+		temps[i] = (char)temperatura;
+	}
+	media = soma/50;
+
+	printf("A media eh: %d\n", media);
+
+	return 0;
+}
