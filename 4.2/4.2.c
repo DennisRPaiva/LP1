162169b2635bc2cb6c5e23b43bb4233d1c26f658
Dennis Paiva <33735664+jacatau23@users.noreply.github.com>
2018-09-30 14:06:33 -0300

    Create 4.2.c

---
+++ 4.2/4.2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int todos_os_primos(int numero)
+{
+   int i;
+   int cont = 0;
+  for (i=1;i<=numero;i++)
+   {
+      if(eh_primo(i))
+      {
+         continue;
+      }
+      cont++;
+   }
+   return cont;
+}
+
+int eh_primo(int primo)
+{
+   int i;
+   for (i=2;i<=sqrt(primo);i++)
+   {
+      if(primo % i == 0)
+      {
+         return 1; // Nao eh primo
+      }
+   }
+   return 0; // Eh primo
+}
+
+int main()
+{
+   int num;
+   int x;
+   printf("Escreva um numero: ");
+   scanf("%d",&num);
+   x = todos_os_primos(num);
+
+   printf("Existem %d numeros primos ate %d.",x,num);
+}
