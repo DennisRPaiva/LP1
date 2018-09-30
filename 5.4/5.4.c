cbd2445c854253b80bfbcc6afdba5704cb02a628
Dennis Paiva <33735664+jacatau23@users.noreply.github.com>
2018-09-30 14:43:43 -0300

    Create 5.4.c

---
+++ 5.4/5.4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void troca(int* conta, int* conta2)
+{
+	int* temp;
+	temp = conta;
+	conta = conta2;
+	conta2 = temp;
+
+	printf("contas trocadas: %d %d.\n",*conta, *conta2);
+}
+
+int main(){
+	int minha_conta,minha_conta2;
+	//int* conta;
+	printf("Digite valores das contas:");
+	scanf("%d %d",&minha_conta,&minha_conta2);
+    printf("contas originais: %d %d.\n",minha_conta, minha_conta2);
+
+    troca(&minha_conta,&minha_conta2);
+}
