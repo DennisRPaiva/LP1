d9d8c69774fdb69cb5e81671275a2ea8542a1197
Dennis Paiva <33735664+jacatau23@users.noreply.github.com>
2018-11-18 14:02:08 -0200

    Update 6.5.c

--- 6.5.c/6.5.c
+++ 6.5.c/6.5.c
@@ -9,33 +9,35 @@ int strlen2(char* str){
     return cont;
 }
 
-void strjoin2(char* dest, char* batman1, char* batman2)
+char* strjoin2(char* batman1, char* batman2)
 {
     int i = 0;
     int tam1 = strlen2(batman1);
     int tam2 = strlen2(batman2);
+    char dest[tam1+tam2+1];
 
     for(i;i<tam1;i++)
     {
         dest[i] = batman1[i];
     }
-    int a = strlen2(dest);
-    for(i = 0;i<tam2;i++)
+    int aux = 0;
+    for(i = tam1,i<tam1+tam2;i++)
     {
-        dest[a+i] = batman2[i];
+        dest[i] = batman2[aux];
+        aux++;
     }
-    //falta botar o /0 no final.
-    //dest[(tam1+tam2)+1] == '\0';
+    dest[tam1+tam2] = '\0';
+    
+    return dest;
 }
 
 
 
 int main()
 {
-    char destino[50];
     char origem1[10] = "abcdef";
     char origem2[12] = "zxywut";
 
-    strjoin2(destino,origem1,origem2);
+   char* destino = strjoin2(origem1,origem2);
     printf("string final: %s\n",destino);
 }
